32eb807309f2b131f5d62bcc996f8bba4a4db2a7
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-06-10 10:02:59 +0200

    implemented login

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
 
 class SessionController extends Controller
 {
@@ -19,7 +22,7 @@ class SessionController extends Controller
      */
     public function create()
     {
-        //
+        return view('auth.login');
     }
 
     /**
@@ -27,7 +30,20 @@ class SessionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.',
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/');
     }
 
     /**
@@ -59,6 +75,8 @@ class SessionController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        Auth::logout();
+
+        return redirect('/');
     }
 }
